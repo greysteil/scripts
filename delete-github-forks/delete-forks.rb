5dde6b966d81bf380c5382636f791fbaf1a884a4
Grey Baker <greysteil@gmail.com>
2023-03-24 15:33:05 -0400

    Clean up delete forks script

--- delete-github-forks/delete-forks.rb
+++ delete-github-forks/delete-forks.rb
@@ -1,22 +1,23 @@
-# List all forks on my GitHub account and delete them
-
+# List all the forks on a GitHub account and delete them
 
 require 'octokit'; nil # Silence a Faraday notice
 
-puts "What is your GitHub Handle?"
+puts "What GitHub handle do you want to list forks on?"
 GITHUB_HANDLE = gets.chomp
 
 puts "What is your GitHub PAT?"
 GITHUB_API_TOKEN = gets.chomp
 
-puts "Dry run? (Any input other than 'no' will trigger a dry run.)"
-DRY_RUN = gets.chomp.downcase != "no"
-
-client = Octokit::Client.new(access_token: GITHUB_API_TOKEN)
-client.auto_paginate = true
+puts "Delete the forks? (Any input other than 'yes' will just do a dry run.)"
+DRY_RUN = gets.chomp.downcase != "yes"
 
 puts "\nOK, the following repos #{ DRY_RUN ? 'would be' : 'were' } deleted:\n"
 
+client = Octokit::Client.new(access_token: GITHUB_API_TOKEN)
+
+# In an ideal world we would lazy-paginate through the repositories, but since
+# the list is generally small fetching the whole lot is probably fine.
+client.auto_paginate = true
 client.repos(GITHUB_HANDLE).each do |repo|
     next unless repo.fork
     next unless repo.owner.login = GITHUB_HANDLE
